6ec0c49ad5c2ebc37f728fdc86fa47ac183efa97
arunkapila <arunj.kapila@gmail.com>
2025-03-13 13:51:41 +0000

    Deprecated VisibleAbilityV2

--- core/src/main/java/com/starshootercity/abilities/VisibleAbilityV2.java
+++ core/src/main/java/com/starshootercity/abilities/VisibleAbilityV2.java
@@ -1,4 +1,8 @@
 package com.starshootercity.abilities;
 
+/**
+ @deprecated Temporary class for translation update, will be removed in a future version
+ */
+@Deprecated(forRemoval = true)
 public interface VisibleAbilityV2 extends VisibleAbility {
 }
